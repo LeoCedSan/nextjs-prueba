a77f5e0089ea76c3df286d8f140b62ffc125831d
TheTzunder30 <leocedsan@campus.monlau.com>
2024-01-31 17:56:14 +0100

    a

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,10 @@
-import {Montserrat} from 'next/font/google'
+import {Inter} from 'next/font/google'
 
-export const montserrat = Montserrat({subsets: ['latin']})
+import {Lusitana} from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin']})
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+})
